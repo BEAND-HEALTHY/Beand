bbc4d0c9d26930b0c9c8490aa87e7f50732bb103
Pedro RD <pedro.ard1212@gmail.com>
2023-06-20 11:56:09 -0500

    js,navbar
    borrado de js
    centrado del fixed, arreglo en modo mobile, color en hover,

--- js/script.js
+++ js/script.js
@@ -1,21 +1,2 @@
-let  menu = document.querySelector('#Menu'); //What?
-let  navegador = document.querySelector('.nav-links');
 
-menu.onclick = () => {
-    menu.classList.toggle('fa-times');
-    navegador.classList.toggle('active');
-}
 
-var swiper = new Swiper(".carousel", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 7500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    loop:true,
-  });
